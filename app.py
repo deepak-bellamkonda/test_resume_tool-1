8ba12f262f54dadda2ce9af17c5a92a6826fba54
piyush-tripathi <81457442+piyush-tripathi@users.noreply.github.com>
2023-10-13 16:32:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,14 +47,14 @@ if st.button("Submit"):
                     #text += page.extract_text()
                     text = "\n".join([text,page.extract_text()])
 
-                st.write(text)
+                #st.write(text)
 
                 #st.write(decoded_content)
             else:
                 st.write("Error occured,please Try again!")
 
             prompt1 = "Look at the resume given.  Show each data under its relevant heading in json format. The resume is :"
-            prompt2 = "Examine the current role person is working in extracted above and identify if it is missing following data :Role Name, Current company, and Duration . If missing any detail, rewrite the current role using blanks for missing data .Only show the question asking for missing details. Don't show a description."
+            prompt2 = "Extract the current role from the resume, and display it in json format including the role, the company, and the duration"
             prompt3 = "Extract the work summary/description from the above and display the following :- 1. Work Description 2. Years of experience.if not ask the user to enter 3. Industries worked in.Then rephrase the 1st point above to include the years of experience, industries worked in,so that it showcases the following values : Personal Impact,Entrepreneurial Drive,Inclusive Leadership,Courageous Change, Problem Solving, Expertise. Do not repeat the values as they are, and it need not include all the values.Do not add any unknown information, do not repeat phrases. Limit the description to less than 50 words"
             prompt4 = "Extract the educational background from this and ask the users if any of the following missing entries Degree,Institution ,GPA/Percentage ,Year for each education are present. and don't give me any extra suggestions "
             prompt5 = "Extract the skills from the above resume and make sure that the skills are relevant for management consulting resumes. Remove the skills that are not relevant. If no skills match the description, ask the user to enter at least three skills. Limit the skills to 4 and each skill should not exceed 3 words.If skills are lacking, suggest new skills."
@@ -89,7 +89,7 @@ if st.button("Submit"):
             st.header(format)
             st.write(outputs)
             
-            #Work Summary
+           ''' #Work Summary
             prompt_3=list_of_prompts[2] + output
             response = openai.Completion.create(
                    engine="text-davinci-002",
@@ -151,7 +151,7 @@ if st.button("Submit"):
             outputs = response.choices[0].text.strip()
             format="Your Extracurriculars are as follows : "
             st.header(format)
-            st.write(outputs)
+            st.write(outputs)'''
 
 
 
