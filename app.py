c2d9eb47ea17b9bd0cd9f0db24a1ce6a15538bd8
piyush-tripathi <81457442+piyush-tripathi@users.noreply.github.com>
2023-10-16 12:33:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,12 +51,13 @@ if st.button("Submit"):
                 st.write("Error occured,please Try again!")
 
             prompt1 = "Look at the resume given.  Show each data under its relevant heading in json format. The resume is :"
-            prompt2 = "Extract and display the details of the person's current role or most recent role, including the Role Name, Company, and Duration in json format. If there's no end date specified for the current role, assume it's an ongoing role. Only display this for one role. The data is : "
-            prompt3 = "Write a first person work profile/summary from the data given so that it showcases the following values : Personal Impact,Entrepreneurial Drive,Inclusive Leadership,Courageous Change, Problem Solving, Expertise. Do not repeat the values as they are, and it need not include all the values. Limit to less than 50 words. Do not use repetitive sentence structures. The data is :"
-            prompt4 = "extract the specified details from the JSON data and format the output so that it lists the Degree, Institute Name, Year of Graduation, and Grade for each educational experience in json. Ensure that you use only the data present in the JSON file and do not create or guess information. The data is : "
-            prompt5 = "Extract the skills from the JSON data and then list up to four skills that are most relevant to a consulting role based on the information provided in the document in json format. If less than 4 relevant skills exist, suggest some skills they can list in a consulting resume in another json format. The data is : "
-            prompt6 = "Extract the extracurricular activities from the above resume. Extracurricular activities are the activities not part of professional or educational activities. These are usually hobbies, and other activities done for fun. Limit to displaying only 5 activities if there are too many extracurriculars. Only show a heading for each extracurricular activity. If there are no such activities,display '''No Extracurriculars''' and ask the user to enter the activities in any of the following categories Cultural,Volunteer,Musical activities,Miscellaneous,Sports"
-            prompt7 = "Extract the work experience from the resume and and put it in chronological order"
+            prompt2 = "Extract and display the most recent role of the person in humand readable format"
+            prompt3 = "Extract and display the details of the person's current role or most recent role, including the Role Name, Company, and Duration in json format. If there's no end date specified for the current role, assume it's an ongoing role. Only display this for one role. The data is : "
+            prompt4 = "Write a first person work profile/summary from the data given so that it showcases the following values : Personal Impact,Entrepreneurial Drive,Inclusive Leadership,Courageous Change, Problem Solving, Expertise. Do not repeat the values as they are, and it need not include all the values. Limit to less than 50 words. Do not use repetitive sentence structures. The data is :"
+            prompt5 = "extract the specified details from the JSON data and format the output so that it lists the Degree, Institute Name, Year of Graduation, and Grade for each educational experience in json. Ensure that you use only the data present in the JSON file and do not create or guess information. The data is : "
+            prompt6 = "Extract the skills from the JSON data and then list up to four skills that are most relevant to a consulting role based on the information provided in the document in json format. If less than 4 relevant skills exist, suggest some skills they can list in a consulting resume in another json format. The data is : "
+            prompt7 = "Extract the extracurricular activities from the above resume. Extracurricular activities are the activities not part of professional or educational activities. These are usually hobbies, and other activities done for fun. Limit to displaying only 5 activities if there are too many extracurriculars. Only show a heading for each extracurricular activity. If there are no such activities,display '''No Extracurriculars''' and ask the user to enter the activities in any of the following categories Cultural,Volunteer,Musical activities,Miscellaneous,Sports"
+            prompt8 = "Extract the work experience from the resume and and put it in chronological order"
           
             #Resume Segmentation
             prompt_1=[
@@ -67,23 +68,24 @@ if st.button("Submit"):
                 model="gpt-3.5-turbo",
                 messages=prompt_1
             )
-            output = response.choices[0]['message']['content']
+            segment = response.choices[0]['message']['content']
     
             #Current Role
-            '''prompt_2=list_of_prompts[1] + output
-            response = openai.Completion.create(
-                   engine="text-davinci-002",
-                   prompt=prompt_2,
-                   max_tokens=1867,  # You can adjust the number of tokens as needed
-                   api_key=api_key
-                   )
-            outputs = response.choices[0].text.strip()
+            prompt_2=[
+                { "role": "system","content": prompt2},
+                {"role": "user","content": segment}
+            ]
+            response = completion.create(
+                model="gpt-3.5-turbo",
+                messages=prompt_1
+            )
+            current_role = response.choices[0]['message']['content']
             format="The current role you are working in :"
             st.header(format)
-            st.write(outputs)
+            st.write(current_role)
             
            #Work Summary
-            prompt_3=list_of_prompts[2] + output
+            '''prompt_3=list_of_prompts[2] + output
             response = openai.Completion.create(
                    engine="text-davinci-002",
                    prompt=prompt_3,
